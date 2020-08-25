bf4a7d18b7d2f2ede22616b7bd10bfac29053450
LINDBURG <monc2170@gmail.com>
2020-08-25 18:10:03 +0900

    중간저장

--- python/test.py
+++ python/test.py
@@ -1,45 +1,59 @@
 import sys
-from itertools import combinations
+from functools import reduce
 from copy import deepcopy
 
-class Castle():
-    def __init__(self, n, m, d, board):
-        self.allocations = list(combinations([i for i in range(m)], 3))
+class Paper():
+    def __init__(self, board):
         self.board = board
-        self.dist = d
-        self.answer = 0
-        for allocation in self.allocations:
-            self.calc(allocation)
+        self.count = -1
+        self.left = [0,5,5,5,5,5]
+        self.match(self.board, self.left, 0, 0)
 
-    def calc(self, allocation):
-        count = 0
-        board = deepcopy(self.board)
-        m = len(board[0])
-        for n in range(len(board),0,-1):
-            remv = [[-30,-30],[-30,-30],[-30,-30]]
-            for i in range(max(0,n-self.dist),n):
-                for j in range(m):
-                    if board[i][j] == 1:
-                        for idx in range(3):
-                            comp = allocation[idx]
-                            dist = n-i + abs(comp-j)
-                            n_dist = n-remv[idx][0] + abs(comp-remv[idx][1])
-                            if dist <= self.dist and dist < n_dist:
-                                remv[idx] = [i,j]
-                            elif dist == n_dist and j <remv[idx][1]:
-                                remv[idx] = [i,j]
-            for i, j in remv:
-                if i != -30 and board[i][j] == 1:
-                    board[i][j] = 0
-                    count += 1
-            
-            if count > self.answer:
-                self.answer = count
+    def match(self, board, left, i, j):
+        ok = 1
+        for i in range(i, 10):
+            for j in range(j, 10):
+                removable = self.check(board, i, j, left)
+                if board[i][j] == 1 and not removable:
+                    ok = 0
+                    break
+                for remove in removable:
+                    nxt_board, nxt_left = self.remove(board, remove, left, i, j)
+                    self.match(nxt_board, nxt_left, i, j)
+            if ok == 0:
+                break
+        if ok == 1:
+            print(left)
+            print(board)
+            if self.count == -1 or 25-sum(left) < self.count:
+                self.count = 25 - sum(left)
 
-N, M, D = map(int,sys.stdin.readline().split(' '))
-board = []
-for _ in range(N):
-    board.append(list(map(int,sys.stdin.readline().split(' '))))
+    def check(self, board, row, col, left):
+        available = []
+        for size in range(1,6):
+            if left[size] > 0 and  row + size < 11 and  col + size < 11:
+                tmp = 1
+                for i in range(row, row+size):
+                    tmp *= reduce(lambda x, y: x*y, board[i][col:col+size])
+                    if tmp == 0:
+                        break
+                if tmp == 1:
+                    available.append(size)
+                else:
+                    break
+        return available
 
-castle = Castle(N, M, D, board)
-print(castle.answer)
+    def remove(self, board, size, left, k, l):
+        board = deepcopy(board)
+        left = deepcopy(left)
+        for i in range(k, k+size):
+            for j in range(l, l+size):
+                board[i][j] = 0
+        left[size] -= 1
+        return board, left
+        
+
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(10)]
+paper = Paper(board)
+print(paper.count)
+print(paper.left)
