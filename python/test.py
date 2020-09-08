cef80afe0e3160261a3bd790fa183f74c672f847
LINDBURG <monc2170@gmail.com>
2020-09-08 23:41:18 +0900

    중간저장

--- python/test.py
+++ python/test.py
@@ -1,25 +1,47 @@
 import sys
-from math import ceil
+from itertools import combinations
 
-class Room():
-    def __init__(self, student, senior, junior):
-        self.student = student
-        self.senior = senior
-        self.junior = junior
-        self.cnt = len(student)
-        self.calc()
+class Lab():
+    def __init__(self, N, M, walls):
+        self.walls = walls
+        self.empty_cnt = -3
+        self.empty_max = 0
+        self.empty = []
+        self.poison = []
+        for i in range(N):
+            for j in range(M):
+                if self.walls[i][j] == 0:
+                    self.empty_cnt += 1
+                    self.empty.append([i,j])
+                elif self.walls[i][j] == 2:
+                    self.poison.append([i,j])
+        self.poison_min = self.empty_cnt
+        
+        for fst, scd, trd in combinations(self.empty, 3):
+            self.run(fst, scd, trd)
 
-    def calc(self):
-        for num in self.student:
-            num -= self.senior
-            if num > 0:
-                self.cnt += ceil(num / self.junior)
+    def run(self, fst, scd, trd):
+        self.walls[fst[0]][fst[1]] = 1
+        self.walls[scd[0]][scd[1]] = 1
+        self.walls[trd[0]][trd[1]] = 1
+        
+        self.corrupted = 0
+        self.queue = self.poison
+        self.spread()
+
+        self.walls[fst[0]][fst[1]] = 0
+        self.walls[scd[0]][scd[1]] = 0
+        self.walls[trd[0]][trd[1]] = 0
+
+    def spread(self):
+        if self.corrupted > self.poison_min:
+            return 0
+        while self.queue:
 
 
 
-N = int(sys.stdin.readline())
-student = list(map(int,sys.stdin.readline().split()))
-senior, junior = map(int, sys.stdin.readline().split())
-room = Room(student, senior, junior)
+N,M = map(int,sys.stdin.readline().split())
+walls = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
+lab = Lab(N, M, walls)
 
-print(room.cnt)
+#print(lab.comb)
